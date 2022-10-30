804f0ed15e0a901c962c34835ec8fc8178ab82f8
Manuel Odendahl <wesen@ruinwesen.com>
2022-10-30 13:19:17 -0400

    :art: Add CSRF_KEY extraction

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ echo "Creating keys"
 mkdir -p /go/keys/keys
 echo "$COOKIES_AUTH_KEY" | base64 -d > /go/keys/keys/cookies_auth.aes256
 echo "$COOKIES_ENC_KEY" | base64 -d > /go/keys/keys/cookies_enc.aes256
+echo "$CSRF_KEY" | base64 -d > /go/keys/keys/csrf.aes256
 echo "$EMAIL_KEY" | base64 -d > /go/keys/keys/email.aes256
 echo "$OAUTH_KEY" | base64 -d > /go/keys/keys/oauth.aes256
 ls -la /go/keys/keys
