052f8be2e5421661cfc69ad3ea35e37dc6eaad4e
Manuel Odendahl <wesen@ruinwesen.com>
2022-10-30 13:12:12 -0400

    :art: Add debug echos to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+echo "Creating config.ini"
 echo "$CONFIG_INI" > /go/config.ini
+ls -la /go/config.ini
+
+echo "Creating keys"
 mkdir -p /go/keys/keys
 echo "$COOKIES_AUTH_KEY" | base64 -d > /go/keys/keys/cookies_auth.aes256
 echo "$COOKIES_ENC_KEY" | base64 -d > /go/keys/keys/cookies_enc.aes256
 echo "$EMAIL_KEY" | base64 -d > /go/keys/keys/email.aes256
 echo "$OAUTH_KEY" | base64 -d > /go/keys/keys/oauth.aes256
+ls -la /go/keys/keys
 
+echo "Starting app"
 cd /go
 ./cmd/writefreely/writefreely db init
 ./cmd/writefreely/writefreely db migrate
